1f0c5b9cc50f7216d0f61ea5fb25cd64f1dfd195
Rhyma <rhymataylor@gmail.com>
2022-09-12 16:57:17 +0530

    Fixed several errors
    Changed UI of questions, fixed Mesh Collider on chest, added script for book

---
+++ Assets/Universe1_BookScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Universe1_BookScript : MonoBehaviour
+
+{
+    public GameObject U1_key1;
+    public GameObject U1_key2;
+    public GameObject U1_key3;
+    public Transform target;
+    public float t;
+    public AudioSource Correct;
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.name == "Player" && !U1_key1.activeSelf && !U1_key2.activeSelf && !U1_key3.activeSelf)
+        {
+            Correct.Play();
+            Destroy(gameObject);
+
+            // StartCoroutine(ExecuteAfterTime());
+        }
+    }
+
+}
